150863f10c77a6a7dd72412c4b1ff334c373d8da
Takeyuki0617 <janjagajan0@gmail.com>
2020-05-06 17:15:36 +0900

    feat: 下記動画を参考にカウントダウンできるように変更　ただし、ボトムナビゲーションにonpressed()が仕掛けられなかったので一時的にオタマジャクシにイベントを設置 https://www.youtube.com/watch?v=tRe8teyf9Nk

--- frog_timer/lib/main.dart
+++ frog_timer/lib/main.dart
@@ -26,28 +26,38 @@ class CountDownTimer extends StatefulWidget {
   _CountDownTimerState createState() => _CountDownTimerState();
 }
 
-class _CountDownTimerState extends State<CountDownTimer> {
+class _CountDownTimerState extends State<CountDownTimer> with TickerProviderStateMixin{
   int _counter = 180;
+  AnimationController controller;
+//  var _remainingTime = "3:00";
+
+  String get timerString {
+    Duration duration = controller.duration * controller.value;
+    return '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
 
   @override
   void initState() {
-    startTimer();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 100)
+    );
     super.initState();
   }
 
-  void startTimer() {
-    Timer.periodic(Duration(seconds: 1),
-        (Timer timer) => setState(
-            () {
-              if(_counter < 1) {
-                timer.cancel();
-              } else {
-                _counter = _counter - 1;
-              }
-            },
-        ),
-    );
-  }
+//  void startTimer() {
+//    Timer.periodic(Duration(seconds: 1),
+//      (Timer timer) => setState(
+//        () {
+//          if(_counter < 1) {
+//            timer.cancel();
+//          } else {
+//            _counter = _counter - 1;
+//          }
+//        },
+//      ),
+//    );
+//  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,17 +70,38 @@ class _CountDownTimerState extends State<CountDownTimer> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display3,
+            AnimatedBuilder(
+              animation: controller,
+              builder: (BuildContext context, Widget child) {
+                return Text(
+                  '$timerString',
+                  style: Theme.of(context).textTheme.display3,
+                );
+              },
+//              child: Text(
+//                '$timerString',
+//                style: Theme.of(context).textTheme.display3,
+//              ),
             ),
-            Container(
-              width: 250,
-              height: 250,
-              child: Image.asset(
-                'images/otamajakushi1.png',
-                fit: BoxFit.contain,
+            InkWell(
+              child: Container(
+                width: 250,
+                height: 250,
+                child: Image.asset(
+                  'images/otamajakushi1.png',
+                  fit: BoxFit.contain,
+                )
               ),
+              onTap: () {
+                if (controller.isAnimating) {
+                  controller.stop(canceled: true);
+                } else {
+                  controller.reverse(
+                      from: controller.value == 0.0
+                          ? 1.0
+                          : controller.value);
+                }
+              },
             )
           ],
         ),
@@ -89,4 +120,5 @@ class _CountDownTimerState extends State<CountDownTimer> {
       ),
     );
   }
+
 }
